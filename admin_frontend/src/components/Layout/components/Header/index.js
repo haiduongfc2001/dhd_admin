ee8cd0d9c64d59a1c4cc87eafd7fa7ebece7b804
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-05-04 22:31:11 +0700

    header

--- admin_frontend/src/components/Layout/components/Header/index.js
+++ admin_frontend/src/components/Layout/components/Header/index.js
@@ -1,31 +1,51 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss"
-import {Button, Stack} from "react-bootstrap";
+import Tippy from '@tippyjs/react/headless';
 import logoDHD from "~/assets/images/logo_dhdadmin.png"
 import {BiBell} from "react-icons/bi";
 import {FiLogOut} from "react-icons/fi";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleXmark, faMagnifyingGlass, faSpinner} from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img
                     src={logoDHD}
-                     alt="logo dhd"
+                    alt="logo dhd"
                     className={cx('logoadmin')}
                 />
-                {/*<Stack direction="horizontal" gap={2}>*/}
-                {/*    <Button as="a" variant="primary">*/}
-                {/*        Button as link*/}
-                {/*    </Button>*/}
-                {/*    <Button as="a" variant="success">*/}
-                {/*        Button as link*/}
-                {/*    </Button>*/}
-                {/*</Stack>*/}
-                {/*Content Header*/}
+                <Tippy
+                    visible={searchResult.length > 0}
+                    render={attrs => (
+                        <div className={cx('search-results')} tabIndex={-1} {...attrs}>
+                            Kết quả
+                        </div>
+                    )
+                    }
+                >
+                    <div className={cx('search')}>
+                        <input
+                            placeholder="Search ..."
+                            spellCheck={false}
+                        />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark}/>
+                        </button>
+                        {/*Loading*/}
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
 
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        </button>
+                    </div>
+                </Tippy>
                 <div className={cx('')}>
                     <BiBell className={cx('icon-bell')}/>
                     <div>
