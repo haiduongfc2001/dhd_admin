c4789da6d4256e063ef6692631cff2fcda831430
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-05-04 22:30:38 +0700

    sidebar

--- admin_frontend/src/components/Layout/DefaultLayout/Sidebar/index.js
+++ admin_frontend/src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import classNames from "classnames/bind";
 import styles from "./Sidebar.module.scss"
-import {FaUserAlt} from "react-icons/fa";
+import {FaUserAlt, FaUserPlus} from "react-icons/fa";
 import {AiOutlineShoppingCart} from "react-icons/ai";
+import {Button} from "react-bootstrap";
+import {BsFillCartCheckFill} from "react-icons/bs";
 
 const cx = classNames.bind(styles)
 const Sidebar = () => {
     return (
         <div className={cx('wrapper')}>
-            <div>
-                <FaUserAlt />
-                <span className={cx('sidebar-users')}>
-                    Users
-                </span>
-            </div>
-            <div>
-                <AiOutlineShoppingCart />
-                <span className={cx('sidebar-users')}>
-                    Products
-                </span>
+            <div className={cx('inner')}>
+                <Button className={cx('sidebar-menu-users', 'selected')}>
+                    <FaUserAlt className={cx('icon-users')}/>
+                    <span className={cx('sidebar-users')}>
+                        Users
+                    </span>
+                </Button>
+                <Button className={cx('sidebar-menu-products')}>
+                    <AiOutlineShoppingCart className={cx('icon-products')}/>
+                    <span className={cx('sidebar-products')}>
+                        Products
+                    </span>
+                </Button>
+                <Button className={cx('sidebar-menu-orders')}>
+                    <BsFillCartCheckFill className={cx('icon-orders')}/>
+                    <span className={cx('sidebar-orders')}>
+                        Orders
+                    </span>
+                </Button>
+                <Button className={cx('sidebar-menu-register')}>
+                    <FaUserPlus className={cx('icon-register')}/>
+                    <span className={cx('sidebar-register')}>
+                        Register
+                    </span>
+                </Button>
             </div>
         </div>
     );
