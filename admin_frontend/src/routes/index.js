84f918c9a223721ae686893ac5ed1b5535d2d839
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-05-06 00:34:38 +0700

    sign in

--- admin_frontend/src/routes/index.js
+++ admin_frontend/src/routes/index.js
@@ -2,12 +2,14 @@
 import Home from "~/pages/Home";
 import Following from "~/pages/Following";
 import CreateDefaultAdmin from "~/components/CreateAccountAdmin/CreateAccountAdmin";
+import SignIn from "~/pages/SignIn/SignIn";
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     // { path: '/upload', component: Upload, layout: null },
     { path: '/account', component: CreateDefaultAdmin },
+    { path: '/signin', component: SignIn, layout: null },
 ]
 
 // Private Routes
