bfd4c6985e4c3543ee758c48357a9fe2071e6b32
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-06-18 12:13:23 +0700

    test code

--- admin_client/src/test.js
+++ admin_client/src/test.js
@@ -1,30 +1,72 @@
 // import {Button} from "react-bootstrap";
-// import React from "react";
+// import React, {useEffect, useState} from "react";
 // import api from "~/api/api";
 //
 //
-// const FilterMovieGenre = ({cx, setMovies}) => {
+// const FilterMovieGenre = ({cx, setGenreMovies, setShowGenreMovies}) => {
+//     const [genresMovies, setGenresMovies] = useState([]);
+//
+//     useEffect(() => {
+//         api.get('/movies/genres')
+//             .then((response) => {
+//                 setGenresMovies(response.data)
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             })
+//     })
 //
 //     const handleMovieGenre = (e) => {
 //         e.preventDefault();
+//         setShowGenreMovies(true);
 //
-//         api.get('/movie/genre/romance')
+//         const genre = e.target.value; // Lấy giá trị của thẻ Button được click
+//         api.get(`/movie/genre/${genre}`)
 //             .then(response => {
-//                 setMovies(response.data);
+//                 setGenreMovies(response.data);
 //             })
 //             .catch(error => {
 //                 console.log(error);
 //             });
-//     }
+//     };
+//
+//     const handleAllMovies = (e) => {
+//         e.preventDefault();
+//
+//         api.get('/movies')
+//             .then(response => {
+//                 setGenreMovies(response.data);
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//             });
+//     };
 //
 //     return (
-//         <div>
-//             <Button
-//                 onClick={handleMovieGenre}
-//                 value="action"
-//             >
-//                 Romance
-//             </Button>
+//         <div className={cx('filter-movie-body')}>
+//             <div className={cx('m-3', 'filter-movie-genre')}>
+//                 <p onClick={handleAllMovies}>
+//                     All Movies
+//                     <span> > </span>
+//                 </p>
+//                 <ul>
+//                     {
+//                         genresMovies.map((genreMovie) => (
+//                             <li
+//                                 key={genreMovie.id}
+//                                 className={'mb-2'}
+//                             >
+//                                 <Button
+//                                     onClick={handleMovieGenre}
+//                                     value={genreMovie.name}
+//                                 >
+//                                     {genreMovie.name}
+//                                 </Button>
+//                             </li>
+//                         ))
+//                     }
+//                 </ul>
+//             </div>
 //         </div>
 //     )
 // }
