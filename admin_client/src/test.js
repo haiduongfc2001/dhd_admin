2f485f9965bbbf25bcc3b9de9b2421bce09244c7
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-06-21 22:14:02 +0700

    test

--- admin_client/src/test.js
+++ admin_client/src/test.js
@@ -1,145 +1,54 @@
-// import {Button, Modal, Form, ModalTitle, FloatingLabel} from "react-bootstrap";
-// import {useState} from "react";
+// import {MDBTable, MDBTableBody, MDBTableHead} from "mdb-react-ui-kit";
 // import api from "~/api/api";
+// import React, {useEffect, useState} from "react";
 //
-// const EditUser = ({cx, user}) => {
-//     const [show, setShow] = useState(false);
-//     const [name, setName] = useState("");
-//     const [email, setEmail] = useState("");
-//     const [phone, setPhone] = useState("");
-//     const [image, setImage] = useState(null);
+// const Ratings = () => {
 //
-//     const handleNameChange = (e) => {
-//         setName(e.target.value);
-//     };
+//     const [movies, setMovies] = useState([]);
 //
-//     const handleEmailChange = (e) => {
-//         setEmail(e.target.value);
-//     };
+//     useEffect(() => {
+//         api.get('/movies/ratings')
+//             .then((response) => {
+//                 setMovies(response.data);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             })
+//     })
 //
-//     const handlePhoneChange = (e) => {
-//         setPhone(e.target.value);
-//     };
-//
-//     const handleImageChange = (e) => {
-//         setImage(e.target.files[0]);
-//     };
-//
-//     const handleShow = () => {
-//         setName(user.name);
-//         setEmail(user.email);
-//         setPhone(user.phone);
-//         setShow(true);
-//     };
-//
-//     const handleClose = () => {
-//         setShow(false);
-//     };
-//
-//     const handleEditUser = async (e) => {
-//         e.preventDefault();
-//
-//         // Tạo một đối tượng FormData để gửi dữ liệu
-//         const formData = new FormData();
-//         formData.append("name", name);
-//         formData.append("email", email);
-//         formData.append("phone", phone);
-//         formData.append("image", image);
-//
-//         if (!name || !email || !phone || !image) {
-//             return;
-//         }
-//
-//         try {
-//
-//             const response = await api.put(`/user/${user._id}`, formData, {
-//                 headers: {
-//                     'Content-Type': 'multipart/form-data',
-//                 },
-//             });
-//             const updatedUser = response.data;
-//             handleClose();
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-//
-//     const editUserForm = [
-//         {
-//             label: "User Name",
-//             type: "text",
-//             id: "name",
-//             value: name,
-//             onChange: handleNameChange,
-//         },
-//         {
-//             label: "User Email",
-//             type: "email",
-//             id: "email",
-//             value: email,
-//             onChange: handleEmailChange,
-//         },
-//         {
-//             label: "User Phone Number",
-//             type: "number",
-//             id: "phone",
-//             value: phone,
-//             onChange: handlePhoneChange,
-//         },
-//         {
-//             label: "Avatar",
-//             type: "file",
-//             id: "image",
-//             onChange: handleImageChange,
-//         },
-//     ]
+//     const tableArray = ['ID', 'Ratings'];
 //
 //     return (
-//         <>
-//             <Button onClick={handleShow}>
-//                 Edit
-//             </Button>
-//
-//             <Modal show={show} centered onHide={handleClose}>
-//                 <Modal.Header closeButton>
-//                     <ModalTitle>Edit User</ModalTitle>
-//                 </Modal.Header>
-//                 <Modal.Body>
-//                     <Form inline="true" className={cx('form-user')}>
-//                         {editUserForm.map((form, index) => (
-//                             <FloatingLabel
-//                                 key={index}
-//                                 label={form.label}
-//                             >
-//                                 <Form.Control
-//                                     type={form.type}
-//                                     id={form.id}
-//                                     value={form.value}
-//                                     onChange={form.onChange}
-//                                 />
-//                             </FloatingLabel>
+//             <div>
+//                 <MDBTable align='middle'>
+//                     <MDBTableHead>
+//                         <tr>
+//                             {tableArray.map((table, index) => (
+//                                 <th key={index} scope='col'>{table}</th>
+//                             ))}
+//                         </tr>
+//                     </MDBTableHead>
+//                     <MDBTableBody>
+//                         {movies.map((movie) => (
+//                             <tr key={movie._id}>
+//                                 <td>{movie.id}</td>
+//                                 <td>
+//                                     {
+//                                         movie.ratings &&
+//                                         movie.ratings.map(r => (
+//                                             <div key={r._id}>
+//                                                     <span>{r.rating}</span>
+//                                                     <span>{r.user && r.user.email}</span>
+//                                             </div>
+//                                         ))
+//                                     }
+//                                 </td>
+//                             </tr>
 //                         ))}
-//                         {
-//                             user.image && (
-//                                 <img
-//                                     src={user.image}
-//                                     alt={user.email}
-//                                 />
-//                             )
-//                         }
-//                     </Form>
-//                 </Modal.Body>
-//                 <Modal.Footer>
-//                     <Button onClick={handleClose}>
-//                         Close
-//                     </Button>
-//                     <Button onClick={handleEditUser}>
-//                         Save
-//                     </Button>
-//                 </Modal.Footer>
-//             </Modal>
-//         </>
-//     );
-// };
+//                     </MDBTableBody>
+//                 </MDBTable>
+//             </div>
+//     )
+// }
 //
-// export default EditUser;
+// export default Ratings;
