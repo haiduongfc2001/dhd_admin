a65af27b6c00f98253f5b0d096f016a73aff6964
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-04 14:41:59 +0700

    add code

--- admin_client/src/components/formatDayMonth.js
+++
@@ -1,7 +0,0 @@
-const getFormattedDate = (date) => {
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    return `${day}/${month}`;
-};
-
-export default getFormattedDate;
--- admin_client/src/hooks/formatTime.js
+++
@@ -1,7 +0,0 @@
-import moment from "moment";
-
-function formatTime(time) {
-    return moment(time).format('DD-MM-YYYY HH:mm:ss');
-}
-
-export default formatTime;
