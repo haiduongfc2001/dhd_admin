f4ede9f03cedf879860e47a5efdcbbdc43a3002e
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-06 23:29:58 +0700

    login, logout and middleware

--- admin_server/src/middleware/Auth.js
+++ admin_server/src/middleware/Auth.js
@@ -11,78 +11,117 @@
 //     }
 // };
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/UserModel");
 
 const authMiddleware = (req, res, next) => {
-    // Get the token from the request header
-    const token = req.headers.authorization;
+  // Get the token from the request header
+  const token = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
-    try {
-        // Verify the token
-        const decodedToken = jwt.verify(token, 'your-secret-key');
+  try {
+    // Verify the token
+    const decodedToken = jwt.verify(token, "your-secret-key");
 
-        // Add the decoded user ID to the request object
-        req.userId = decodedToken.userId;
+    // Add the decoded user ID to the request object
+    req.userId = decodedToken.userId;
 
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 const isLogin = (req, res, next) => {
-    // Check if user is logged in
-    if (req.session && req.session.user) {
-        // User is logged in, proceed to the next middleware or route handler
-        next();
-    } else {
-        // User is not logged in, redirect to login page or send an error response
-        res.status(401).json({ message: 'Unauthorized' });
-    }
+  // Check if user is logged in
+  if (req.session && req.session.user) {
+    // User is logged in, proceed to the next middleware or route handler
+    next();
+  } else {
+    // User is not logged in, redirect to login page or send an error response
+    res.status(401).json({ message: "Unauthorized" });
+  }
 };
 
 // Middleware for admin authentication
 const isAdminLogin = (req, res, next) => {
-    // Check if admin is logged in
-    if (req.session  && req.session.adminId) {
-        // Admin is logged in, proceed to the next middleware or route handler
-        next();
-    } else {
-        // Admin is not logged in or not authorized, redirect to login page or send an error response
-        res.status(401).json({ message: 'Unauthorized' });
-    }
+  // Check if admin is logged in
+  if (req.session && req.session.adminId) {
+    // Admin is logged in, proceed to the next middleware or route handler
+    next();
+  } else {
+    // Admin is not logged in or not authorized, redirect to login page or send an error response
+    res.status(401).json({ message: "Unauthorized" });
+  }
 };
 
 // Middleware for logging out
 const isLogout = (req, res) => {
-    // Destroy the session to log out the user
-    req.session.destroy((err) => {
-        if (err) {
-            console.log(err);
-        }
-        // Redirect to the login page or send a success response
-        res.status(200).json({ message: 'Logged out successfully' });
-    });
+  // Destroy the session to log out the user
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    // Redirect to the login page or send a success response
+    res.status(200).json({ message: "Logged out successfully" });
+  });
 };
 
-// const isLogout = async (req, res, next) => {
-//     try {
-//         if (req.session._id) {
-//             return res.status(200).json({ message: 'Already logged in' });
-//         }
-//         next();
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// };
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Không có token nào được cung cấp" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token không hợp lệ" });
+    }
+
+    req.userId = decoded.user_id;
+    next();
+  });
+};
+
+const authenticateAdmin = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Không có token nào được cung cấp" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token không hợp lệ" });
+    }
+
+    req.userId = decoded.id;
+
+    // Kiểm tra nếu là admin
+    User.findById(req.userId, (error, user) => {
+      if (error || !user || user.is_admin !== 1) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      next();
+    });
+  });
+};
 
 module.exports = {
-    isLogin,
-    isAdminLogin,
-    isLogout,
-    authMiddleware
-}
+  isLogin,
+  isAdminLogin,
+  isLogout,
+  authMiddleware,
+
+  authenticateUser,
+  authenticateAdmin,
+};
