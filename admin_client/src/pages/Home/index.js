5c01ff63540e5182358674cf84d5fb1bb002f7a2
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-08 00:59:47 +0700

    fix error of too many requests to the server

--- admin_client/src/pages/Home/index.js
+++ admin_client/src/pages/Home/index.js
@@ -1,138 +1,137 @@
-import styles from './Home.module.scss'
-import classNames from "classnames/bind"
+import styles from "./Home.module.scss";
+import classNames from "classnames/bind";
 import BreadcrumbExample from "~/components/Layout/components/BreadcrumbExample/BreadcrumbExample";
-import ListMovies from "~/pages/Movies/Movies";
-import {Card} from "react-bootstrap";
-import {BiMoviePlay} from "react-icons/bi";
-import {FaUserAlt} from "react-icons/fa";
-import {BsFillCartCheckFill} from "react-icons/bs";
-import {AiFillStar} from "react-icons/ai";
-import {useEffect, useState} from "react";
+// import ListMovies from "~/pages/Movies/Movies";
+import { Card } from "react-bootstrap";
+import { BiMoviePlay } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
+import { useEffect, useState } from "react";
 import api from "~/api/api";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import PieChartUsers from "~/components/PieChart/PieChartUsers";
 import LineChart from "~/components/LineChart/LineChart";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Home() {
-    const [countMovies, setCountMovies] = useState(0);
-    const [countUsers, setCountUsers] = useState(0);
-    const [countCompanies, setCountCompanies] = useState(0);
-    const [countRatings, setCountRatings] = useState('');
+  const [countMovies, setCountMovies] = useState(0);
+  const [countUsers, setCountUsers] = useState(0);
+  const [countCompanies, setCountCompanies] = useState(0);
+  const [countRatings, setCountRatings] = useState("");
 
-    const getCountFromAPI = async (getAPI, setData) => {
-        try {
-            const response = await api.get(getAPI);
-            setData(response.data.length);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const getCountFromAPI = async (getAPI, setData) => {
+    try {
+      const response = await api.get(getAPI);
+      setData(response.data.length);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        getCountFromAPI('/movies', setCountMovies);
-    }, []);
+  useEffect(() => {
+    getCountFromAPI("/movies", setCountMovies);
+  }, []);
 
-    useEffect(() => {
-        getCountFromAPI('/users', setCountUsers);
-    }, []);
+  useEffect(() => {
+    getCountFromAPI("/users", setCountUsers);
+  }, []);
 
-    useEffect(() => {
-        getCountFromAPI('/movies/companies', setCountCompanies);
-    }, []);
+  useEffect(() => {
+    getCountFromAPI("/movies/companies", setCountCompanies);
+  }, []);
 
-    useEffect(() => {
-        api.get('/movies/user-vote')
-            .then(response => {
-                const {countVote} = response.data;
-                setCountRatings(countVote);
-            })
-            .catch(err => {
-                console.error(err);
-            })
-    }, []);
+  useEffect(() => {
+    api
+      .get("/movies/user-vote")
+      .then((response) => {
+        const { countVote } = response.data;
+        setCountRatings(countVote);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-    const cardsData = [
-        {
-            card: 'card-movies',
-            cardIcon: 'card-movies-icon',
-            icon: <BiMoviePlay/>,
-            cardText: 'card-movies-text',
-            count: countMovies,
-            text: 'Movies',
-            cardLink: '/movies'
-        },
-        {
-            card: 'card-users',
-            cardIcon: 'card-users-icon',
-            icon: <FaUserAlt/>,
-            cardText: 'card-users-text',
-            count: countUsers,
-            text: 'Users',
-            cardLink: '/users'
-        },
-        {
-            card: 'card-companies',
-            cardIcon: 'card-companies-icon',
-            icon: <BsFillCartCheckFill/>,
-            cardText: 'card-companies-text',
-            count: countCompanies,
-            text: 'Companies',
-            cardLink: '/companies'
-        },
-        {
-            card: 'card-ratings',
-            cardIcon: 'card-ratings-icon',
-            icon: <AiFillStar/>,
-            cardText: 'card-ratings-text',
-            count: countRatings,
-            text: 'Ratings',
-            cardLink: '/ratings'
-        },
-    ]
+  const cardsData = [
+    {
+      card: "card-movies",
+      cardIcon: "card-movies-icon",
+      icon: <BiMoviePlay />,
+      cardText: "card-movies-text",
+      count: countMovies,
+      text: "Movies",
+      cardLink: "/movies",
+    },
+    {
+      card: "card-users",
+      cardIcon: "card-users-icon",
+      icon: <FaUserAlt />,
+      cardText: "card-users-text",
+      count: countUsers,
+      text: "Users",
+      cardLink: "/users",
+    },
+    {
+      card: "card-companies",
+      cardIcon: "card-companies-icon",
+      icon: <BsFillCartCheckFill />,
+      cardText: "card-companies-text",
+      count: countCompanies,
+      text: "Companies",
+      cardLink: "/companies",
+    },
+    {
+      card: "card-ratings",
+      cardIcon: "card-ratings-icon",
+      icon: <AiFillStar />,
+      cardText: "card-ratings-text",
+      count: countRatings,
+      text: "Ratings",
+      cardLink: "/ratings",
+    },
+  ];
 
-    return (
-        <div className={cx('wrapper')}>
-            <BreadcrumbExample/>
+  return (
+    <div className={cx("wrapper")}>
+      <BreadcrumbExample />
 
-            <div className={cx('card-dashboard')}>
-                {
-                    cardsData.map((c, index) => (
-                        <Card className={cx(`${c.card}`)} key={index}>
-                            <div className={cx(`${c.cardIcon}`, 'm-3')}>{c.icon}</div>
-                            <div className={cx('m-3', `${c.cardText}`)}>
-                                <Link to={`${c.cardLink}`}>
-                                    <h2>{c.count} {c.text}</h2>
-                                </Link>
-                            </div>
-                        </Card>
-                    ))
-                }
-
-                {/*<Card className={cx('card-movies')}>*/}
-                {/*    <div className={cx('card-movies-icon', 'm-3')}><BiMoviePlay/></div>*/}
-                {/*    <div className={cx('m-3', 'card-movies-text')}>*/}
-                {/*        <h2>{countMovies} Movies</h2>*/}
-                {/*    </div>*/}
-                {/*</Card>*/}
-
+      <div className={cx("card-dashboard")}>
+        {cardsData.map((c, index) => (
+          <Card className={cx(`${c.card}`)} key={index}>
+            <div className={cx(`${c.cardIcon}`, "m-3")}>{c.icon}</div>
+            <div className={cx("m-3", `${c.cardText}`)}>
+              <Link to={`${c.cardLink}`}>
+                <h2>
+                  {c.count} {c.text}
+                </h2>
+              </Link>
             </div>
+          </Card>
+        ))}
 
-            <div className={cx('users-chart', 'mt-5', 'mb-5')}>
-                <div className={cx('users-chart-column')}>
-                    <h1>Số người dùng đăng ký theo ngày</h1>
-                    <LineChart/>
-                </div>
+        {/*<Card className={cx('card-movies')}>*/}
+        {/*    <div className={cx('card-movies-icon', 'm-3')}><BiMoviePlay/></div>*/}
+        {/*    <div className={cx('m-3', 'card-movies-text')}>*/}
+        {/*        <h2>{countMovies} Movies</h2>*/}
+        {/*    </div>*/}
+        {/*</Card>*/}
+      </div>
 
-                <div className={cx('users-chart-pie')}>
-                    <h1>Thống kê tài khoản</h1>
-                    <PieChartUsers/>
-                </div>
-            </div>
-
+      <div className={cx("users-chart", "mt-5", "mb-5")}>
+        <div className={cx("users-chart-column")}>
+          <h1>Số người dùng đăng ký theo ngày</h1>
+          <LineChart />
         </div>
-    )
+
+        <div className={cx("users-chart-pie")}>
+          <h1>Thống kê tài khoản</h1>
+          <PieChartUsers />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- admin_client/src/pages/Movies/AddMovie.js
+++ admin_client/src/pages/Movies/AddMovie.js
@@ -1,197 +1,196 @@
-import React, {useState} from "react";
-import {IoMdAddCircle} from "react-icons/io";
-import {Button, FloatingLabel, Form, ModalTitle} from "react-bootstrap";
-import {toast} from "react-toastify";
+import React, { useState } from "react";
+import { IoMdAddCircle } from "react-icons/io";
+import { Button, FloatingLabel, Form, ModalTitle } from "react-bootstrap";
+import { toast } from "react-toastify";
 import Modal from "react-bootstrap/Modal";
 import api from "~/api/api";
 
-function AddMovie({ cx }) {
+function AddMovie({ cx, setMovies }) {
+  const [show, setShow] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const [show, setShow] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
+  const [title, setTitle] = useState("");
+  const [overview, setOverview] = useState("");
+  const [poster_path, setPoster_Path] = useState("");
 
-    const [title, setTitle] = useState('');
-    const [overview, setOverview] = useState('');
-    const [poster_path, setPoster_Path] = useState('');
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleClose = () => {
+    setShow(false);
+    setTitle("");
+    setOverview("");
+    setPoster_Path("");
+    setErrorMessage("");
+  };
 
-    const handleShow = () => {
-        setShow(true);
-    };
-    const handleClose = () => {
-        setShow(false);
-        setTitle('');
-        setOverview('');
-        setPoster_Path('');
-        setErrorMessage('');
-    };
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
-    };
+  const handleOverviewChange = (e) => {
+    setOverview(e.target.value);
+  };
 
-    const handleOverviewChange = (e) => {
-        setOverview(e.target.value);
-    };
+  const handlePoster_PathChange = (e) => {
+    setPoster_Path(e.target.value);
+  };
 
-    const handlePoster_PathChange = (e) => {
-        setPoster_Path(e.target.value);
-    };
+  const handleAddMovie = async (e) => {
+    e.preventDefault();
 
-    const handleAddMovie = async (e) => {
-        e.preventDefault();
+    if (!title || !overview || !poster_path) {
+      toast.error("Xin vui lòng nhập đầy đủ thông tin!");
+      return;
+    }
 
-        if (!title || !overview || !poster_path) {
-            toast.error("Xin vui lòng nhập đầy đủ thông tin!");
-            return;
-        }
-
-        const data = {
-            title: title,
-            overview: overview,
-            poster_path: poster_path,
-        }
-
-        try {
-            const response = await api.post('/movie', data);
-
-            console.log(response.data);
-
-            setShow(false);setTitle('');
-            setOverview('');
-            setPoster_Path('');
-            setErrorMessage('');
-
-            toast.success('Movie added successfully!', {
-                position: "bottom-center",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
-            });
-        } catch (error) {
-            // setErrorMessage('Failed to add movie');
-            toast.error('Error adding movie!', {
-                position: "bottom-center",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
-            });
-            console.error(error);
-        }
+    const data = {
+      title: title,
+      overview: overview,
+      poster_path: poster_path,
     };
 
-    const addMovieForm = [
-        {
-            label: (
-                <>
-                    Movie Name{" "}
-                    <span style={{color: "red"}} dangerouslySetInnerHTML={{__html: "*"}}/>
-                </>
-            ),
-            type: "text",
-            id: "title",
-            value: title,
-            onChange: handleTitleChange,
-        },
-        {
-            label: (
-                <>
-                    Movie Overview{" "}
-                    <span
-                        style={{color: "red"}}
-                        dangerouslySetInnerHTML={{__html: "*"}}
-                    />
-                </>
-            ),
-            type: "text",
-            id: "overview",
-            value: overview,
-            onChange: handleOverviewChange,
-        },
-        {
-            label: (
-                <>
-                    Movie Poster Path{" "}
-                    <span
-                        style={{color: "red"}}
-                        dangerouslySetInnerHTML={{__html: "*"}}
-                    />
-                </>
-            ),
-            type: "text",
-            id: "poster_path",
-            value: poster_path,
-            onChange: handlePoster_PathChange,
-        },
-    ]
+    try {
+      const response = await api.post("/movie", data);
 
-    return (
+      console.log(response.data);
+
+      setShow(false);
+      setTitle("");
+      setOverview("");
+      setPoster_Path("");
+      setErrorMessage("");
+
+      // Gọi lại API để lấy danh sách movie và cập nhật state
+      const getMoviesResponse = await api.get("/movies");
+      setMovies(getMoviesResponse.data);
+
+      toast.success("Movie added successfully!", {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } catch (error) {
+      // setErrorMessage('Failed to add movie');
+      toast.error("Error adding movie!", {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      console.error(error);
+    }
+  };
+
+  const addMovieForm = [
+    {
+      label: (
         <>
-            <Button
-                size="lg"
-                variant="primary"
-                className={"mb-4"}
-                onClick={handleShow}
-            >
-                <IoMdAddCircle className={cx('icon-action')}/>
-            </Button>
-
-            <Modal
-                show={show}
-                backdrop={"static"}
-                centered
-                onHide={handleClose}
-            >
-                <Modal.Header closeButton>
-                    <ModalTitle>
-                        <h2>Add Movie</h2>
-                    </ModalTitle>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form className={cx('form-movie')}>
-                        {addMovieForm.map((form, index) => (
-                            <FloatingLabel
-                                key={index}
-                                label={form.label}
-                                className="mr-sm-2 mb-2 min-height-50"
-                            >
-                                <Form.Control
-                                    autoFocus={form.id === 'title'}
-                                    id={form.id}
-                                    type={form.type}
-                                    value={form.value}
-                                    onChange={form.onChange}
-                                    size="lg"
-                                    style={{minHeight: "50px"}}
-                                    required
-                                />
-                            </FloatingLabel>
-                        ))}
-                    </Form>
-
-                    {errorMessage && <p className={'text-danger'}>{errorMessage}</p>}
-
-                </Modal.Body>
-                <Modal.Footer
-                    className={cx('modal-footer')}
-                >
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleAddMovie}>
-                        Add Movie
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+          Movie Name{" "}
+          <span
+            style={{ color: "red" }}
+            dangerouslySetInnerHTML={{ __html: "*" }}
+          />
         </>
-    )
+      ),
+      type: "text",
+      id: "title",
+      value: title,
+      onChange: handleTitleChange,
+    },
+    {
+      label: (
+        <>
+          Movie Overview{" "}
+          <span
+            style={{ color: "red" }}
+            dangerouslySetInnerHTML={{ __html: "*" }}
+          />
+        </>
+      ),
+      type: "text",
+      id: "overview",
+      value: overview,
+      onChange: handleOverviewChange,
+    },
+    {
+      label: (
+        <>
+          Movie Poster Path{" "}
+          <span
+            style={{ color: "red" }}
+            dangerouslySetInnerHTML={{ __html: "*" }}
+          />
+        </>
+      ),
+      type: "text",
+      id: "poster_path",
+      value: poster_path,
+      onChange: handlePoster_PathChange,
+    },
+  ];
+
+  return (
+    <>
+      <Button
+        size="lg"
+        variant="primary"
+        className={"mb-4"}
+        onClick={handleShow}
+      >
+        <IoMdAddCircle className={cx("icon-action")} />
+      </Button>
+
+      <Modal show={show} backdrop={"static"} centered onHide={handleClose}>
+        <Modal.Header closeButton>
+          <ModalTitle>
+            <h2>Add Movie</h2>
+          </ModalTitle>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className={cx("form-movie")}>
+            {addMovieForm.map((form, index) => (
+              <FloatingLabel
+                key={index}
+                label={form.label}
+                className="mr-sm-2 mb-2 min-height-50"
+              >
+                <Form.Control
+                  autoFocus={form.id === "title"}
+                  id={form.id}
+                  type={form.type}
+                  value={form.value}
+                  onChange={form.onChange}
+                  size="lg"
+                  style={{ minHeight: "50px" }}
+                  required
+                />
+              </FloatingLabel>
+            ))}
+          </Form>
+
+          {errorMessage && <p className={"text-danger"}>{errorMessage}</p>}
+        </Modal.Body>
+        <Modal.Footer className={cx("modal-footer")}>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleAddMovie}>
+            Add Movie
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default AddMovie;
