cc8dde465eecca491c9e944b6303894453b1dfcb
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-13 00:45:10 +0700

    Update index.js

--- admin_server/src/index.js
+++ admin_server/src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log("HTTP Method: " + req.method + " , URL - " + req.url);
+  console.log("HTTP Method: " + req.method + ", URL - " + req.url);
   next();
 });
 
