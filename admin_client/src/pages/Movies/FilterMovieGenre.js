da51633ac6b7cf156bd60afb561acb9aec18a83e
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-13 17:20:45 +0700

    Update FilterMovieGenre.js

--- admin_client/src/pages/Movies/FilterMovieGenre.js
+++ admin_client/src/pages/Movies/FilterMovieGenre.js
@@ -2,6 +2,7 @@ import { Button } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import api from "~/api/api";
 import { BsCheckAll } from "react-icons/bs";
+import { AiOutlineDown } from "react-icons/ai";
 
 const FilterMovieGenre = ({ cx, setGenreMovies, setShowGenreMovies }) => {
   const [genresMovies, setGenresMovies] = useState([]);
@@ -46,54 +47,32 @@ const FilterMovieGenre = ({ cx, setGenreMovies, setShowGenreMovies }) => {
   };
 
   return (
-    <div className={cx("filter-movie-body")}>
-      <div className={cx("m-3", "filter-movie-genre")}>
-        <p onClick={handleAllMovies}>
-          Thể loại
-          <span> > </span>
-        </p>
-        <ul>
-          <li className={"mb-2"}>
-            <Button onClick={handleAllMovies} value={"All Movies"}>
-              All Movies
-              <BsCheckAll className={"ms-2"} />
-            </Button>
-          </li>
-          {genresMovies
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .map((genreMovie) => (
-              <li key={genreMovie.id} className={"mb-2"}>
-                <Button onClick={handleMovieGenre} value={genreMovie.name}>
-                  {genreMovie.name}
-                </Button>
-              </li>
-            ))}
-        </ul>
-      </div>
+    <div className={cx("m-3", "filter-movie-genre")}>
+      <p onClick={handleAllMovies}>
+        Thể loại
+        <span>
+          {" "}
+          <AiOutlineDown />{" "}
+        </span>
+      </p>
+      <ul>
+        <li className={"mb-2"}>
+          <Button onClick={handleAllMovies} value={"All Movies"}>
+            All Movies
+            <BsCheckAll className={"ms-2"} />
+          </Button>
+        </li>
+        {genresMovies
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((genreMovie) => (
+            <li key={genreMovie.id} className={"mb-2"}>
+              <Button onClick={handleMovieGenre} value={genreMovie.name}>
+                {genreMovie.name}
+              </Button>
+            </li>
+          ))}
+      </ul>
     </div>
-
-    // <div className={cx('filter-movie-body')}>
-    //     <p>
-    //         <Button onClick={handleAllMovies}>
-    //             All Movies
-    //             <span> > </span>
-    //         </Button>
-    //     </p>
-    //     <div className={cx('m-3', 'filter-movie-genre')}>
-    //         {
-    //             genresMovies.map((genreMovie) => (
-    //                 <Button
-    //                     onClick={handleMovieGenre}
-    //                     key={genreMovie.name}
-    //                     className={'m-3'}
-    //                     value={genreMovie.name}
-    //                 >
-    //                     {genreMovie.name}
-    //                 </Button>
-    //             ))
-    //         }
-    //     </div>
-    // </div>
   );
 };
 
