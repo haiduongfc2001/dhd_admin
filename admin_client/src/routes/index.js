57e510cca5893008f8044196ba7397f2a13376ca
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-13 17:21:55 +0700

    remove CreateDefaultAdmin

--- admin_client/src/routes/index.js
+++ admin_client/src/routes/index.js
@@ -1,6 +1,5 @@
 // Public Routes
 import Home from "~/pages/Home";
-import CreateDefaultAdmin from "~/components/CreateAccountAdmin/CreateAccountAdmin";
 import SignIn from "~/pages/SignIn/SignIn";
 // import Test from "~/test";
 import Products from "~/pages/Products/Products";
@@ -14,28 +13,27 @@ import Movies from "~/pages/Movies/Movies";
 import Ratings from "~/pages/Ratings/Ratings";
 
 export const publicRoutes = [
-    { path: '/admin/login', component: SignIn, layout: null },
-    { path: '/admin/forgot-password', component: ForgotPassword, layout: null },
-    { path: '/admin/reset-password', component: ResetPassword, layout: null },
-    // { path: '/test', component: Test, layout: null },
-    { path: '/not-found', component: NotFound, layout: null },
-]
+  { path: "/admin/login", component: SignIn, layout: null },
+  { path: "/admin/forgot-password", component: ForgotPassword, layout: null },
+  { path: "/admin/reset-password", component: ResetPassword, layout: null },
+  // { path: '/test', component: Test, layout: null },
+  { path: "/not-found", component: NotFound, layout: null },
+];
 
 // Private Routes
 export const privateRoutes = [
-    { path: '/', component: Home },
-    { path: '/account', component: CreateDefaultAdmin },
-    { path: '/products', component: Products },
-    { path: '/movies', component: Movies },
-    { path: '/users', component: Users },
-    { path: '/companies', component: Companies },
-    { path: '/supplier', component: Suppliers },
-    { path: '/ratings', component: Ratings },
-]
+  { path: "/", component: Home },
+  { path: "/products", component: Products },
+  { path: "/movies", component: Movies },
+  { path: "/users", component: Users },
+  { path: "/companies", component: Companies },
+  { path: "/supplier", component: Suppliers },
+  { path: "/ratings", component: Ratings },
+];
 
 export const errorRoutes = [
-    { path: '/not-found', component: NotFound },
-    // ... other error routes
+  { path: "/not-found", component: NotFound },
+  // ... other error routes
 ];
 
 // export default [
